2e9aca7de38df0824f27af99a9a57daaa541c488
rafikfartas <rafikawm8@gmail.com>
2023-08-21 15:06:36 +0100

    datatables replace cdn with gem imoprt

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,15 @@
 // Entry point for the build script in your package.json
 
-
 import "@hotwired/turbo-rails"
 import "./controllers"
+import "flowbite/dist/flowbite.turbo.js";
+import jQuery from 'jquery'
+window.jQuery = jQuery
+window.$ = jQuery
+import 'datatables.net-dt'
+
+
+
+
+
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
-  config.assets.debug = true
+  #  config.assets.debug = true
 
 
   # Enable/disable caching. By default caching is disabled.
